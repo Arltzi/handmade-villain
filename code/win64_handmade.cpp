91ad96713dbd5d86440d01d50e483a96ab048620
Lev Zitron <lev@zitron.ca>
2025-09-15 01:19:20 -0700

    init commit

---
+++ code/win64_handmade.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance, 
+    PWSTR pCmdLine, 
+    int nCmdShow) 
+{
+    // Register window class
+    // Create the main window
+    // Show the window
+    // Run the message loop
+    return 0;
+}
